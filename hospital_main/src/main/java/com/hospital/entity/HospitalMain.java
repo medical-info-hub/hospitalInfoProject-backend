4a21f145a4ddf59799fdb8f167d613424cedab84
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2026-04-06 16:35:36 +0900

    fix: HospitalMain 미사용 JTS Point import 제거

--- hospital_main/src/main/java/com/hospital/entity/HospitalMain.java
+++ hospital_main/src/main/java/com/hospital/entity/HospitalMain.java
@@ -3,7 +3,6 @@ package com.hospital.entity;
 import java.util.Set;
 
 import org.hibernate.annotations.DynamicUpdate;
-import org.locationtech.jts.geom.Point;
 
 // JPA 관련 임포트 추가
 import jakarta.persistence.Column;
